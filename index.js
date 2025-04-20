8e0e5499c8ed48574b6df91cad451fb5fb608cb7
Liana-Geo <l.geonjyan@hbk-bs.de>
2025-04-20 18:39:00 +0200

    x
    x

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
 // Classifier Variable
 let classifier;
 // Model URL
-// HERE
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/6eL2SJyis/';
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/eQxMXMly8/';
 
 // Video
 let video;
-let flippedVideo;
 // To store the classification
 let label = '';
 
+// Images for each motion
+let images = {
+    "Motion1": "images/squish1.png",
+    "Motion2": "images/squish2.png",
+    "Motion3": "images/squish3.png",
+    "Motion4": "images/squish4.png",
+};
+
 // Load the model first
 function preload() {
-	classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-	console.log(classifier);
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 }
 
 function setup() {
-	createCanvas(320, 260);
-	// Create the video
-	video = createCapture(VIDEO);
-	video.size(320, 240);
-	video.hide();
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
 
-	// Start classifying
-	classifyVideo();
+    // Start classifying
+    classifyVideo();
 }
 
 function draw() {
-	background(0);
-	// Draw the video
-	image(video, 0, 0);
+    background(0);
+    // Draw the video
+    image(video, 0, 0);
 
-	// Draw the label
-	fill(255);
-	textSize(16);
-	textAlign(CENTER);
-	text(label, width / 2, height - 4);
+    // Display the corresponding image for the detected motion
+    if (label in images) {
+        let img = loadImage(images[label], () => {
+            image(img, 0, 0, width, height);
+        });
+    }
+
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
 }
 
 // Get a prediction for the current video frame
 function classifyVideo() {
-	classifier.classify(video, gotResult);
+    classifier.classify(video, gotResult);
 }
 
 // When we get a result
-function gotResult(results) {
-	console.log(results);
-	// The results are in an array ordered by confidence.
-	// console.log(results[0]);
-	label = results[0].label;
-	// Classifiy again!
-	classifyVideo();
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    // The results are in an array ordered by confidence.
+    label = results[0].label;
+    // Classify again!
+    classifyVideo();
 }
